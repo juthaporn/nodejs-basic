bf0386a277ca3800c84433f047ae47b138431f05
juthaporn <jutha.1995@gmail.com>
2019-09-21 12:14:48 +0700

    JSclass Room

---
+++ 1-const-let-var.js
@@ -0,0 +1,24 @@
+// const a = "a";
+// a = "b";
+//consatic
+// let a = "a";
+// a = "b";
+// console.log(a);
+//dynamic
+// var a = "a";
+// a = "c";
+// console.log(a);
+
+// let a = "a";
+// {
+//   let a = "b";
+//   console.log(1, a);
+// }
+// console.log(2, a);
+
+var a = "a";
+{
+  var a = "b";
+  console.log(1, a);
+}
+console.log(2, a);
---
+++ 2-if-for.js
@@ -0,0 +1,42 @@
+// const a = "a";
+// if (a === "a") {
+//   console.log("It,s a");
+// } else {
+//   console.log("not a");
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 0;
+// let sum = 0;
+// while (i < 1001) {
+//   sum = sum + i;
+//   i++;
+// }
+// console.log(sum); //sum = 500500
+
+let a = 1; //int a =1
+let b = 1.1; //float b = 1.1
+let c = "hello"; //string c = "hello"
+let d = true; //bool d = true
+let e = false; //bool e = false
+let f = ["a", "b", "c"]; //string *f = ["a","b","c"]
+f[0] = "f";
+console.log(f);
+let h = [];
+let student = {
+  id: "61101655",
+  "name-lastname": "Pummanee",
+  faculty: "SWE"
+};
+console.log(student["name-lastname"]);
+student["name-lastname"] = "Juthaporn";
+console.log(student["name-lastname"]);
